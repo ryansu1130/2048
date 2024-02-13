d0b9d74439f22c5aa2931131c79f4dde17464a20
Ryan Su <suxi@oregonstate.edu>
2024-02-12 17:41:59 -0800

    minor bug fix

--- src/GameControl.jsx
+++ src/GameControl.jsx
@@ -2,7 +2,7 @@ import "./GameControl.css"
 export default function GameControl({reset}){
     return(
         <div id="gameControlContainer">
-            <button>New Game</button>
+            <button onClick={reset}>New Game</button>
             <button>Finish For Me</button>
         </div>
     )
--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -1,15 +1,25 @@
 import "./ScoreBoard.css";
 import GameControl from "./GameControl";
-export default function ScoreBoard({reset}) {
+export default function ScoreBoard({ reset, score }) {
+  let total = 0;
+  const totalScore = () => {
+    score.map((value, idx) => {
+      if(value >= total){
+        total = value;
+      }
+    });
+  };
+  totalScore();
+
   return (
     <>
       <div id="scoreBoardContainer">
         <h1>2048+</h1>
         <button>
-          Score <p>512</p>
+          Score <p>{total}</p>
         </button>
         <button>
-          Best <p>2048</p>
+          Best <p>{total}</p>
         </button>
         <GameControl reset={reset} />
       </div>
